b8efcdacf3b2082a54d59ea99c5a258a4085e762
dasiFraind <dasif461@gmail.com>
2021-10-14 15:52:55 +0300

    note

--- src/components/SinglePhoto.js
+++ src/components/SinglePhoto.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function SinglePhoto() {
     const url = localStorage.getItem('currentImg');
 
+    // dfsdfs
     return (
             <div className="d-flex justify-content-center align-items-center">
                 <img src={url} alt={url}/>
